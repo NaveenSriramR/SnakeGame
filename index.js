d1c7a60fd415b838c22c157fa6ccffa3d12d3ccc
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2022-01-08 13:02:48 +0530

    project fixed

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //Import the necessary libraries/declare the necessary objects
+var path =require('path')
 var express = require("express");
 // var myParser = require("body-parser");
 var app = express();
-app.get("/", function(req, res){
-    console.log(req.url)
-    res.send("Hello")
-})
 app.use(express.urlencoded({ extended: true }));
-app.post("/snake-game", function (request, response) {
+
+app.get("/", function(req, res){
+    // console.log(req.url)
+    res.sendFile(path.join(__dirname,'\game.html'))
+})
+app.post("/snakeGame", function (request, response) {
     console.log("POST")
     response.send("POST")
     console.log(request.body); //This prints the JSON document received (if it is a JSON document)
@@ -15,4 +17,4 @@ app.post("/snake-game", function (request, response) {
 
 //Start the server and make it listen for connections on port 8080
 
-app.listen(8080);
+app.listen(5000);
